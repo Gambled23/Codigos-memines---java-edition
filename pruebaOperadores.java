663ccd15031b4417ab6d46aa0826b9a5b87b81d4
Cesar <ipog71@gmail.com>
2022-03-07 16:59:50 -0600

    operadores

---
+++ pruebaOperadores.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class pruebaOperadores {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        float numero1, numero2, resultado;
+        System.out.println("Digite 2 numeros: \n");
+        numero1 = entrada.nextFloat();
+        numero2 = entrada.nextFloat();
+        //Suma
+        resultado = numero1 + numero2;
+        System.out.println("El resultado de la suma es: " + resultado);
+        //Resta
+        resultado = numero1 - numero2;
+        System.out.println("El resultado de la resta es: " + resultado);
+        //Multiplicacion
+        resultado = numero1 * numero2;
+        System.out.println("El resultado de la multiplicacion es: " + resultado);
+        //Division
+        resultado = numero1 / numero2;
+        System.out.println("El resultado de la divison es: " + resultado);
+        //Mod
+        resultado = numero1 % numero2;
+        System.out.println("El residuo de la division es: " + resultado);
+    }
+}
