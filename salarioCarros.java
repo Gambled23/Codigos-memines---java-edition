1ae47d457c5271b94818ccd6e05272f6d1bc6dfc
Cesar <ipog71@gmail.com>
2022-03-11 18:17:14 -0600

    Salario carros

---
+++ salarioCarros.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class salarioCarros {
+    public static void main(String[] args) {
+        double salarioFinal = 0;
+        int carrosVendidos;
+        double aux1;
+        double aux2 = 0;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Cuantos carros vendiste? ");
+        carrosVendidos = entrada.nextInt();
+        int i = 0;
+        while (i < carrosVendidos) {
+            System.out.println("Cual es el precio del carro " + (i + 1) + "?");
+            aux1 = entrada.nextDouble();
+            aux1 = aux1 * 0.05;
+            aux2 = aux2 + aux1;
+            i++;
+        }
+        salarioFinal = 1000 + (150 * carrosVendidos) + aux2;
+        System.out.println("Tu salario final es de: $" + salarioFinal);
+    }
+}
