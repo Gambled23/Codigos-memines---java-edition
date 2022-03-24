468d846a85f49928557258b6580363917699549a
Cesar <ipog71@gmail.com>
2022-03-23 18:31:46 -0600

    inicios POO

--- pruebaDeObjetos/src/persona.java
+++ pruebaDeObjetos/src/persona.java
@@ -1,3 +1,13 @@
-public class persona {
-    
+public class Persona {
+    int edad;
+    String nombre;
+    String colorDePiel;
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        persona1.edad = 18;
+        persona1.nombre = "Pablo";
+        persona1.colorDePiel = "negro";
+        System.out.println(persona1.nombre + " tiene " + persona1.edad + " anios y es un " + persona1.colorDePiel);
+    }
 }
